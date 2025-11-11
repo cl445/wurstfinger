045db08b4d0dd816521978a4daac00ac43abab46
Claas Flint <claasflint@yfx.de>
2025-11-11 15:46:41 +0100

    Remove TextShowcaseView reference from app

--- wurstfinger/wurstfingerApp.swift
+++ wurstfinger/wurstfingerApp.swift
@@ -10,13 +10,10 @@ import SwiftUI
 @main
 struct wurstfingerApp: App {
     @State private var showScreenshotMode = ProcessInfo.processInfo.arguments.contains("SCREENSHOT_MODE")
-    @State private var showTextMode = ProcessInfo.processInfo.arguments.contains("TEXT_SCREENSHOT_MODE")
 
     var body: some Scene {
         WindowGroup {
-            if showTextMode {
-                TextShowcaseView()
-            } else if showScreenshotMode {
+            if showScreenshotMode {
                 KeyboardShowcaseView()
             } else {
                 ContentView()
